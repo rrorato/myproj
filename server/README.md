e2d8adafd4728cb405f6f1f2c7ecf160f9720e92
Riccardo Rorato <Riccardo.Rorato@external.eni.com>
2024-05-31 19:56:31 +0200

    action parsing done

--- server/README.md
+++ server/README.md
@@ -7,3 +7,22 @@ Build
 -----
 
     $ rebar3 compile
+
+
+Run
+----
+
+    $ rebar3 shell
+
+
+Test
+----
+    $ rebar3 eunit
+    $ rebar3 shell
+    open the python notebook client.ipynb and run the different sections to test different features
+
+
+Release
+----
+
+    rebar3 release
--- server/src/server_app.erl
+++ server/src/server_app.erl
@@ -3,6 +3,7 @@
 
 -export([start/2, stop/1]).
 
+
 start(_StartType, _StartArgs) ->
     ServPid = start_server(),
     {ok, ServPid}.
@@ -11,6 +12,9 @@ stop(_State) ->
     ok.
 
 start_server() ->
+    ets:new(allowedUsers, [named_table, protected, set, {keypos, 1}]),
+    ets:insert(allowedUsers, {<<"riccardo">>, allowed}),
+    ets:insert(allowedUsers, {<<"alessandro">>, allowed}),
     Pid = spawn_link(fun() ->
         {ok, Listen} = gen_tcp:listen(8080, [binary, {active, false}]),
         spawn(fun() -> acceptor(Listen) end),
@@ -30,8 +34,37 @@ handle(Socket) ->
             io:format("Quitted: ~n", []),
             gen_tcp:close(Socket);
         {tcp, Socket, Msg} ->
-            io:format("Recieved: ~p~n", [Msg]),
-            gen_tcp:send(Socket, Msg),
+            %io:format("Recieved: ~p~n", [Msg]),
+            Res = parse_input(Msg),
+            %io:format("Sending: ~p~n", [Res]),
+            gen_tcp:send(Socket, Res),
             handle(Socket)
     end.
 
+parse_input(ActionInfo) ->
+    ActionInfoParsed = jsx:decode(ActionInfo),
+    case is_valid_action_info(ActionInfoParsed) of
+        false ->
+            {error, formatError};
+        true ->
+            Name = maps:get(<<"name">>, ActionInfoParsed),
+            case ets:lookup(allowedUsers, Name) of
+                [] ->
+                    {error, userNotAllowedError};
+                [_] ->
+                    Action = maps:get(<<"action">>, ActionInfoParsed),
+                    ActionSpecs = maps:get(<<"actionSpecs">>, ActionInfoParsed),
+                    action(Name, Action, ActionSpecs)
+            end
+    end.
+
+is_valid_action_info(ActionInfo) when is_map(ActionInfo) ->
+    maps:is_key(<<"name">>, ActionInfo) andalso
+    maps:is_key(<<"action">>, ActionInfo) andalso
+    maps:is_key(<<"actionSpecs">>, ActionInfo);
+is_valid_action_info(_) ->
+    false.
+
+action(Name, Action, ActionSpecs) ->
+    io:format("TAKING ACTION: ~n", []),
+    "actionDone".
