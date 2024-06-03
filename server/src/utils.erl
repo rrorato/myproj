930c0b7ed7c471ef5eb9d38c3656ab7c23f6eec2
Riccardo Rorato <Riccardo.Rorato@external.eni.com>
2024-06-03 21:40:47 +0200

    private rooms

--- server/src/utils.erl
+++ server/src/utils.erl
@@ -1,12 +1,6 @@
 -module(utils).
--export([tab2binary/1, build_dict/2]).
+-export([build_dict/2]).
 
-tab2binary(Tab) ->
-    TabList = ets:tab2list(Tab),
-    TabConcat = [Elem || {Elem, _, _} <- TabList],
-    TabString = lists:flatten(io_lib:format("~p", [TabConcat])),
-    TabBinary = iolist_to_binary(TabString),
-    TabBinary.
 
 build_dict(State, Info) ->
     #{<<"state">> => list_to_binary(State), <<"info">> => list_to_binary(Info)}.
